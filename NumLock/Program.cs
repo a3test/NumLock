8edc6d846c30a5648fffc07e9b721ea264ef731b
a3test <a3test@163.com>
2015-08-02 14:02:10 +0800

    Done

---
+++ NumLock/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace NumLock
+{
+    internal class Program
+    {
+        private const int KEYEVENTF_EXTENDEDKEY = 0x1;
+        private const int KEYEVENTF_KEYUP = 0x2;
+
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+
+        private static void Main(string[] args)
+        {
+            keybd_event((byte) Keys.NumLock, 0, KEYEVENTF_EXTENDEDKEY, 0);
+            keybd_event((byte) Keys.NumLock, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+        }
+    }
+}
